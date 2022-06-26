fd4df2342d38c6fc8c557683b3592021a8aa05db
Sheldon Fekete <jsbf@cin.ufpe.br>
2022-06-26 04:38:48 -0300

    Exclusão do pacote next-images

--- next.config.js
+++ next.config.js
@@ -7,8 +7,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-const withImages = require('next-images')
-module.exports = withImages({
-    esModule: true
-})
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+
+import { Container, Row } from 'react-bootstrap'
 
 const Login: NextPage = () => {
     return (
@@ -7,9 +10,17 @@ const Login: NextPage = () => {
             <Head>
                 <title>Login</title>
             </Head>
-            <main>
-                <h1>Login aqui</h1>
-            </main>
+
+            <aside className="container">
+                <div className="row">
+                    <Image
+                        src="/src/assets/analyze-1.jpg"
+                        alt="Picture of the author"
+                        width="350px"
+                        height="300px"
+                    />
+                </div>
+            </aside>
         </div>
     )
 }
