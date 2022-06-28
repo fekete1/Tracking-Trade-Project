cc387c1db6f495dad708ec68b6b3c2d1d8176c70
Sheldon Fekete <jsbf@cin.ufpe.br>
2022-06-28 08:11:15 -0300

    Adicionado tags SSRProvider nas páginas que utilizam bootstrap

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row, SSRProvider } from 'react-bootstrap'
+
 import { BarChart } from '../components/BarChart'
 import { HeatMapChart } from '../components/HeatMapChart'
 import { LineChart } from '../components/LineChart'
@@ -9,53 +10,55 @@ import { TopMenu } from '../components/TopMenu'
 
 const Dashboard: NextPage = () => {
     return (
-        <div>
-            <Head>
-                <title>Dashboard</title>
-            </Head>
+        <SSRProvider>
+            <div>
+                <Head>
+                    <title>Dashboard</title>
+                </Head>
 
-            <SideMenu>
-                <TopMenu />
-                <Container fluid>
-                    <Row xs={1} md={2}>
-                        <Col className="products-infos" xs={12} md={6} lg={4}>
-                            <div>Informações do produto</div>
-                            <div>Informações do produto nas lojas</div>
-                        </Col>
+                <SideMenu>
+                    <TopMenu />
+                    <Container fluid>
+                        <Row xs={1} md={2}>
+                            <Col className="products-infos" xs={12} md={6} lg={4}>
+                                <div>Informações do produto</div>
+                                <div>Informações do produto nas lojas</div>
+                            </Col>
 
-                        <Col className="products-charts" xs={12} md={6} lg={8}>
-                            <div>Menu complicadinho de fazer</div>
-                            <Row xs={1} md={1} lg={2}>
-                                <Col>
-                                    <LineChart />
-                                </Col>
-                                <Col>
-                                    <LineChart />
-                                </Col>
-                            </Row>
+                            <Col className="products-charts" xs={12} md={6} lg={8}>
+                                <div>Menu complicadinho de fazer</div>
+                                <Row xs={1} md={1} lg={2}>
+                                    <Col>
+                                        <LineChart />
+                                    </Col>
+                                    <Col>
+                                        <LineChart />
+                                    </Col>
+                                </Row>
 
-                            <Row xs={1} md={1} lg={2}>
-                                <Col>
-                                    <HeatMapChart />
-                                </Col>
-                                <Col>
-                                    <HeatMapChart />
-                                </Col>
-                            </Row>
+                                <Row xs={1} md={1} lg={2}>
+                                    <Col>
+                                        <HeatMapChart />
+                                    </Col>
+                                    <Col>
+                                        <HeatMapChart />
+                                    </Col>
+                                </Row>
 
-                            <Row xs={1} md={1} lg={2}>
-                                <Col>
-                                    <BarChart />
-                                </Col>
-                                <Col>
-                                    <BarChart />
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Container>
-            </SideMenu>
-        </div>
+                                <Row xs={1} md={1} lg={2}>
+                                    <Col>
+                                        <BarChart />
+                                    </Col>
+                                    <Col>
+                                        <BarChart />
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Container>
+                </SideMenu>
+            </div>
+        </SSRProvider>
     )
 }
 
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,7 +6,7 @@ import { FaUser } from 'react-icons/fa'
 import { RiLockPasswordFill } from 'react-icons/ri'
 import { AiOutlineEyeInvisible, AiFillEye } from 'react-icons/ai'
 
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row, SSRProvider } from 'react-bootstrap'
 import { useState } from 'react'
 
 const Login: NextPage = () => {
@@ -17,54 +17,56 @@ const Login: NextPage = () => {
     }
 
     return (
-        <div>
-            <Head>
-                <title>Login</title>
-            </Head>
+        <SSRProvider>
+            <div>
+                <Head>
+                    <title>Login</title>
+                </Head>
 
-            <Container fluid className="" id="login-main-container">
-                <Row>
-                    <Col lg="6" className="d-none d-lg-flex" id="login-aside-section">
-                        <div className="login-aside-content">
-                            <img src="/assets/analyze-2.jpg" alt="Ilustração de dados" />
-                            <div className="login-aside-text">
-                                <strong>Tenha controle dos dados da sua empresa</strong>
-                                <p>Observando precisamente os dados dos seus produtos</p>
+                <Container fluid className="" id="login-main-container">
+                    <Row>
+                        <Col lg="6" className="d-none d-lg-flex" id="login-aside-section">
+                            <div className="login-aside-content">
+                                <img src="/assets/analyze-2.jpg" alt="Ilustração de dados" />
+                                <div className="login-aside-text">
+                                    <strong>Tenha controle dos dados da sua empresa</strong>
+                                    <p>Observando precisamente os dados dos seus produtos</p>
+                                </div>
                             </div>
-                        </div>
-                    </Col>
+                        </Col>
 
-                    <Col lg="6" className="" id="login-auth-section">
-                        <h1>Faça login</h1>
-                        <p className="separator">Entre em uma conta para continuar</p>
+                        <Col lg="6" className="" id="login-auth-section">
+                            <h1>Faça login</h1>
+                            <p className="separator">Entre em uma conta para continuar</p>
 
-                        <form>
-                            <label>
-                                <FaUser size={20} />
-                            </label>
-                            <input type="text" placeholder="login" name="login" />
+                            <form>
+                                <label>
+                                    <FaUser size={20} />
+                                </label>
+                                <input type="text" placeholder="login" name="login" />
 
-                            <label>
-                                <RiLockPasswordFill size={20} />
-                            </label>
+                                <label>
+                                    <RiLockPasswordFill size={20} />
+                                </label>
 
-                            <div className="password-area">
-                                <input type={isHidePassword ? 'text' : 'password'} placeholder="senha" name="password" />
+                                <div className="password-area">
+                                    <input type={isHidePassword ? 'text' : 'password'} placeholder="senha" name="password" />
 
-                                <button type="button" className="btn-hide-password" onClick={toggleHidePassword}>
-                                    {isHidePassword ? <AiOutlineEyeInvisible /> : <AiFillEye />}
+                                    <button type="button" className="btn-hide-password" onClick={toggleHidePassword}>
+                                        {isHidePassword ? <AiOutlineEyeInvisible /> : <AiFillEye />}
+                                    </button>
+                                </div>
+
+                                <button type="submit" className="login-submit">
+                                    <FiLogIn style={{ margin: 4 }} />
+                                    Entrar
                                 </button>
-                            </div>
-
-                            <button type="submit" className="login-submit">
-                                <FiLogIn style={{ margin: 4 }} />
-                                Entrar
-                            </button>
-                        </form>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+                            </form>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </SSRProvider>
     )
 }
 
